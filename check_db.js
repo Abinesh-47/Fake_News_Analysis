eeb17fbd3f5d338d41bfb3bb213338b25dfa0fa5
Abinesh-47 <iabinesh47@gmail.com>
2026-03-21 17:06:30 +0530

    Cleanup: Remove temporary debug scripts

--- check_db.js
+++
@@ -1,5 +0,0 @@
-import Database from 'better-sqlite3';
-const db = new Database('analytics.db');
-const schema = db.prepare("SELECT sql FROM sqlite_master WHERE type='table' AND name='reports'").get();
-console.log('REPORTS TABLE SCHEMA:', schema || 'NOT FOUND');
-db.close();
